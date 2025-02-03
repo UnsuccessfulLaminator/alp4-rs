8c363e9c6cccdbe56237b56a61107a6e38f5f88f
UnsuccessfulLaminator <>
2025-02-03 14:14:55 +0000

    Initial commit

---
+++ src/alp.rs
@@ -0,0 +1,234 @@
+use crate::alp_binding::*;
+use libloading::Library;
+use strum_macros::FromRepr;
+use std::ffi::{OsStr, c_long};
+use std::error::Error;
+use std::fmt::{self, Formatter, Display};
+
+
+
+macro_rules! alp_call {
+    ($lib:expr, $name:expr, $sig:ty; $($args:expr),*) => {
+        unsafe {
+            if let Ok(f) = $lib.get::<$sig>($name.as_bytes()) {
+                let ret = f($($args),*) as i64;
+
+                if ret == ALP_OK as i64 { Ok(()) }
+                else {
+                    Err(AlpError::from_repr(ret).unwrap_or(AlpError::Unknown))
+                }
+            }
+            else { panic!("couldn't get symbol {}", $name) }
+        }
+    }
+}
+
+type AlpDevAllocFn = unsafe extern fn(c_long, c_long, *mut ALP_ID) -> c_long;
+type AlpDevFreeFn = unsafe extern fn(ALP_ID) -> c_long;
+type AlpDevInquireFn = unsafe extern fn(ALP_ID, c_long, *mut c_long) -> c_long;
+type AlpSeqAllocFn = unsafe extern fn(ALP_ID, c_long, c_long, *mut ALP_ID) -> c_long;
+type AlpSeqFreeFn = unsafe extern fn(ALP_ID, ALP_ID) -> c_long;
+type AlpSeqPutFn = unsafe extern fn(ALP_ID, ALP_ID, c_long, c_long, *const u8) -> c_long;
+type AlpProjStartContFn = unsafe extern fn(ALP_ID, ALP_ID) -> c_long;
+type AlpProjHaltFn = unsafe extern fn(ALP_ID) -> c_long;
+type AlpProjInquireExFn = unsafe extern fn(ALP_ID, c_long, *mut tAlpProjProgress) -> c_long;
+type AlpProjInquireFn = unsafe extern fn(ALP_ID, c_long, *mut c_long) -> c_long;
+
+#[repr(i64)]
+#[derive(Copy, Clone, Debug, PartialEq, FromRepr)]
+pub enum AlpError {
+    NotOnline = ALP_NOT_ONLINE as i64,
+    NotIdle = ALP_NOT_IDLE as i64,
+    NotAvailable = ALP_NOT_AVAILABLE as i64,
+    NotReady = ALP_NOT_READY as i64,
+    ParameterInvalid = ALP_PARM_INVALID as i64,
+    AddressInvalid = ALP_ADDR_INVALID as i64,
+    MemoryFull = ALP_MEMORY_FULL as i64,
+    SequenceInUse = ALP_SEQ_IN_USE as i64,
+    Halted = ALP_HALTED as i64,
+    InitFail = ALP_ERROR_INIT as i64,
+    CommunicationFail = ALP_ERROR_COMM as i64,
+    DeviceRemoved = ALP_DEVICE_REMOVED as i64,
+    NotConfigured = ALP_NOT_CONFIGURED as i64,
+    LoaderVersion = ALP_LOADER_VERSION as i64,
+    PoweredDown = ALP_ERROR_POWER_DOWN as i64,
+    DriverVersion = ALP_DRIVER_VERSION as i64,
+    SdramInitFail = ALP_SDRAM_INIT as i64,
+    ConfigMismatch = ALP_CONFIG_MISMATCH as i64,
+    Unknown = ALP_ERROR_UNKNOWN as i64
+}
+
+impl Display for AlpError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), fmt::Error> {
+        write!(f, "{self:?}")
+    }
+}
+
+impl Error for AlpError {}
+
+
+
+type AlpResult<T> = Result<T, AlpError>;
+
+
+
+pub struct Alp {
+    lib: Library
+}
+
+impl Alp {
+    pub fn from_path<P: AsRef<OsStr>>(path: P) -> Result<Self, String> {
+        let lib = unsafe { Library::new(path) }.map_err(|e| e.to_string())?;
+
+        Ok(Self { lib })
+    }
+
+    pub fn allocate_device(&self, id: Option<u64>) -> AlpResult<AlpDevice> {
+        let id = id.unwrap_or(0) as c_long;
+        let mut ret_id = 0;
+
+        alp_call!(self.lib, "AlpDevAlloc", AlpDevAllocFn; id, 0, &mut ret_id)?;
+
+        Ok(AlpDevice {
+            lib: &self.lib,
+            id: ret_id
+        })
+    }
+}
+
+
+
+pub struct AlpDevice<'a> {
+    lib: &'a Library,
+    id: ALP_ID
+}
+
+impl<'a> Drop for AlpDevice<'a> {
+    fn drop(&mut self) {
+        alp_call!(self.lib, "AlpDevFree", AlpDevFreeFn; self.id)
+            .expect("couldn't free ALP device");
+    }
+}
+
+impl<'a> AlpDevice<'a> {
+    pub fn allocate_sequence(&self, bitplanes: usize, images: usize)
+    -> AlpResult<AlpSequence> {
+        let mut id = 0;
+        let bitplanes = bitplanes as c_long;
+        let images = images as c_long;
+        
+        alp_call!(
+            self.lib, "AlpSeqAlloc", AlpSeqAllocFn;
+            self.id, bitplanes, images, &mut id
+        )?;
+
+        Ok(AlpSequence {
+            lib: &self.lib,
+            dev: self,
+            id
+        })
+    }
+
+    pub fn display_size(&self) -> AlpResult<(usize, usize)> {
+        let (mut width, mut height) = (0, 0);
+
+        alp_call!(
+            self.lib, "AlpDevInquire", AlpDevInquireFn;
+            self.id, ALP_DEV_DISPLAY_WIDTH as c_long, &mut width
+        )?;
+        
+        alp_call!(
+            self.lib, "AlpDevInquire", AlpDevInquireFn;
+            self.id, ALP_DEV_DISPLAY_HEIGHT as c_long, &mut height
+        )?;
+
+        Ok((width as usize, height as usize))
+    }
+
+    pub fn halt(&self) -> AlpResult<()> {
+        alp_call!(self.lib, "AlpProjHalt", AlpProjHaltFn; self.id)
+    }
+
+    pub fn current_sequence_id(&self) -> AlpResult<Option<u64>> {
+        let mut progress = tAlpProjProgress {
+            CurrentQueueId: 0,
+            SequenceId: 0,
+            nWaitingSequences: 0,
+            nSequenceCounter: 0,
+            nSequenceCounterUnderflow: 0,
+            nFrameCounter: 0,
+            nPictureTime: 0,
+            nFramesPerSubSequence: 0,
+            nFlags: 0
+        };
+
+        alp_call!(
+            self.lib, "AlpProjInquireEx", AlpProjInquireExFn;
+            self.id, ALP_PROJ_PROGRESS as c_long, &mut progress
+        )?;
+
+        let projecting = (progress.nFlags & ALP_FLAG_QUEUE_IDLE) == 0;
+
+        Ok(projecting.then_some(progress.SequenceId as u64))
+    }
+
+    pub fn is_projecting(&self) -> AlpResult<bool> {
+        let mut val = 0;
+
+        alp_call!(
+            self.lib, "AlpProjInquire", AlpProjInquireFn;
+            self.id, ALP_PROJ_STATE as c_long, &mut val
+        )?;
+
+        Ok(val == ALP_PROJ_ACTIVE as c_long)
+    }
+}
+
+
+
+pub struct AlpSequence<'a> {
+    lib: &'a Library,
+    dev: &'a AlpDevice<'a>,
+    id: ALP_ID
+}
+
+impl<'a> Drop for AlpSequence<'a> {
+    fn drop(&mut self) {
+        let current_seq = self.dev.current_sequence_id()
+            .expect("couldn't get current ALP sequence ID");
+
+        if current_seq == Some(self.id as u64) {
+            self.halt().expect("couldn't halt ALP device projection");
+        }
+
+        alp_call!(self.lib, "AlpSeqFree", AlpSeqFreeFn; self.dev.id, self.id)
+            .expect("couldn't free ALP sequence");
+    }
+}
+
+impl<'a> AlpSequence<'a> {
+    pub fn id(&self) -> u64 {
+        self.id as u64
+    }
+
+    pub fn put(&self, offset: usize, n: usize, data: &[u8]) -> AlpResult<()> {
+        let offset = offset as c_long;
+        let n = n as c_long;
+
+        alp_call!(
+            self.lib, "AlpSeqPut", AlpSeqPutFn;
+            self.dev.id, self.id, offset, n, data.as_ptr()
+        )
+    }
+
+    pub fn start_cont(&self) -> AlpResult<()> {
+        alp_call!(
+            self.lib, "AlpProjStartCont", AlpProjStartContFn;
+            self.dev.id, self.id
+        )
+    }
+
+    pub fn halt(&self) -> AlpResult<()> {
+        self.dev.halt()
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,4 @@
+mod alp_binding;
+mod alp;
+
+pub use alp::*;
